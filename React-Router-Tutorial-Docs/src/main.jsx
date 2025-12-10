9681dddea31ef1813b618565a857714a5a925ebf
Pratik <pratik.student.14@gmail.com>
2025-12-10 13:36:48 +0000

    Full React Router Tutorial project

---
+++ React-Router-Tutorial-Docs/src/main.jsx
@@ -0,0 +1,54 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "../error-page";
+import Contact, { loader as contactLoader } from "./routes/contact";
+import Root, { loader as rootLoader, action as rootAction } from "./routes/root";
+import EditContact, { action as editAction } from "./routes/edit";
+import { action as deleteAction } from "./routes/destroy";
+import Index from "./routes/index";
+import { action as contactAction} from "./routes/contact";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        errorElement: <ErrorPage />,
+        children: [
+          { index: true, element: <Index /> },
+          {
+            path: "contacts/:contactId",
+            element: <Contact />,
+            loader: contactLoader,
+            action: contactAction,
+          },
+          {
+            path: "contacts/:contactId/edit",
+            element: <EditContact />,
+            loader: contactLoader,
+            action: editAction,
+          },
+          {
+            path: "contacts/:contactId/destroy",
+            action: deleteAction,
+            errorElement: <div>Oops! There was an error.</div>,
+          },
+        ],
+      },
+    ],
+  },
+]);
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
